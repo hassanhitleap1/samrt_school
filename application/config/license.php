ba08ca1823428c50289d78e18a4fec622d997789
hasan kiwan <hassankiwan92@gmail.com>
2020-10-10 00:19:09 +0300

    first commit

---
+++ application/config/license.php
@@ -0,0 +1,8 @@
+<?php
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+$config['envato_market_purchase_code'] = '';
+$config['envato_market_username']      = '';
+$config['SSLK'] = 'Y92LS9-HQ4LJP-6HQWZF-di8xQ0Zrd0lPNGUzcUNPVVBBZDdFTzNhTE0weXI3N1grQVFXY3RpMGk2Yz0=';
